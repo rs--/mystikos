a874b76e4ade40f980613ad2b17bd2b4e5c8ba6e
Rob Sanchez <rob.sanchez@microsoft.com>
2021-11-10 02:35:05 -0800

    run dpkg configure if installation fails

--- .jenkins/scripts/solutions/azure-config.sh
+++ .jenkins/scripts/solutions/azure-config.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y azure-cli
+sudo apt-get install -y azure-cli || sudo dpkg --configure -a
 az login --service-principal -u $SERVICE_PRINCIPAL_ID -p $SERVICE_PRINCIPAL_PASSWORD --tenant $TENANT_ID >> /dev/null
 az account set -s $AZURE_SUBSCRIPTION_ID
 az vm identity assign -g $JENKINS_RESOURCE_GROUP -n $(hostname) --identities $MYSTIKOS_MANAGED_ID >> /dev/null
